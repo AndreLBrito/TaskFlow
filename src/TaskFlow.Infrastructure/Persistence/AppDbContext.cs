f7c938d547db679fd8afa71d17ec6da55c9a3a2d
Andre Lucas De Brito <andrelucasdebrito@gmail.com>
2026-05-28 17:25:56 -0300

    feat: adiciona contexto principal da aplicação

---
+++ src/TaskFlow.Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using TaskFlow.Domain.Entities;
+
+namespace TaskFlow.Infrastructure.Persistence;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Workspace> Workspaces => Set<Workspace>();
+
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+}
