226f930bfb75e56a74d5dc44e4ca0530fe76d618
Andre Lucas De Brito <andrelucasdebrito@gmail.com>
2026-05-29 11:26:26 -0300

    feat: adiciona comando de criação de workspace

---
+++ src/TaskFlow.Application/Features/Workspaces/CreateWorkspace/CreateWorkspaceCommand.cs
@@ -0,0 +1,8 @@
+using MediatR;
+
+namespace TaskFlow.Application.Features.Workspaces.CreateWorkspace;
+
+public record CreateWorkspaceCommand(
+    string Name,
+    string? Description
+) : IRequest<Guid>;
