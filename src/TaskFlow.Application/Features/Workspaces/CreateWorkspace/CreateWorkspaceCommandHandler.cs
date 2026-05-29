412ff325504498b7f07bcf175ea4a9f474c52f24
Andre Lucas De Brito <andrelucasdebrito@gmail.com>
2026-05-29 11:34:52 -0300

    feat: adiciona handler para criação de workspace

---
+++ src/TaskFlow.Application/Features/Workspaces/CreateWorkspace/CreateWorkspaceCommandHandler.cs
@@ -0,0 +1,31 @@
+using MediatR;
+using TaskFlow.Application.Interfaces;
+using TaskFlow.Domain.Entities;
+
+namespace TaskFlow.Application.Features.Workspaces.CreateWorkspace;
+
+public class CreateWorkspaceCommandHandler : IRequestHandler<CreateWorkspaceCommand, Guid>
+{
+    private readonly IWorkspaceRepository _workspaceRepository;
+
+    public CreateWorkspaceCommandHandler(
+        IWorkspaceRepository workspaceRepository)
+    {
+        _workspaceRepository = workspaceRepository;
+    }
+
+    public async Task<Guid> Handle(
+        CreateWorkspaceCommand request,
+        CancellationToken cancellationToken)
+    {
+        var workspace = new Workspace(
+            request.Name,
+            request.Description);
+
+        await _workspaceRepository.AddAsync(
+            workspace,
+            cancellationToken);
+
+        return workspace.Id;
+    }
+}
