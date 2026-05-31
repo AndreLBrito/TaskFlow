3cf9f72f27518bdb203efab3677940fe4b22a565
Andre Lucas De Brito <andrelucasdebrito@gmail.com>
2026-05-31 19:30:57 -0300

    refactor: adiciona viewmodels para workspace

---
+++ src/TaskFlow.Web/ViewModels/Workspaces/CreateWorkspaceViewModel.cs
@@ -0,0 +1,8 @@
+namespace TaskFlow.Web.ViewModels.Workspaces;
+
+public class CreateWorkspaceViewModel
+{
+    public string? Name { get; set; }
+
+    public string? Description { get; set; }
+}
---
+++ src/TaskFlow.Web/ViewModels/Workspaces/UpdateWorkspaceViewModel.cs
@@ -0,0 +1,10 @@
+namespace TaskFlow.Web.ViewModels.Workspaces;
+
+public class UpdateWorkspaceViewModel
+{
+    public Guid Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Description { get; set; }
+}
