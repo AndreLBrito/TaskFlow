b659b7c34d73c7eced3e7242c61f2c1dae2aae92
Andre Lucas De Brito <andrelucasdebrito@gmail.com>
2026-06-01 15:58:39 -0300

    feat: adiciona interface de repositório para quadro

---
+++ src/TaskFlow.Application/Interfaces/IBoardRepository.cs
@@ -0,0 +1,18 @@
+using TaskFlow.Domain.Entities;
+
+namespace TaskFlow.Application.Interfaces;
+
+public interface IBoardRepository
+{
+    Task AddAsync(
+        Board board,
+        CancellationToken cancellationToken);
+
+    Task<Board?> GetByIdAsync(
+        Guid id,
+        CancellationToken cancellationToken);
+
+    Task<IReadOnlyList<Board>> GetByWorkspaceIdAsync(
+        Guid workspaceId,
+        CancellationToken cancellationToken);
+}
