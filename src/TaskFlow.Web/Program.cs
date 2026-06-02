d2acca6624f784e8d6c6f0a5c31d827b246320ff
Andre Lucas De Brito <andrelucasdebrito@gmail.com>
2026-06-02 15:59:42 -0300

    refactor: reorganizado a chamada de dependencias

--- src/TaskFlow.Web/Program.cs
+++ src/TaskFlow.Web/Program.cs
@@ -1,61 +1,64 @@
-using Microsoft.EntityFrameworkCore;
 using Serilog;
 using TaskFlow.Application.DependencyInjection;
 using TaskFlow.Infrastructure.DependencyInjection;
-using TaskFlow.Infrastructure.Persistence;
 using TaskFlow.Web.Filters;
 using TaskFlow.Web.Mapping;
 
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Host.UseSerilog();
-
-// Add services to the container.
-builder.Services.AddControllersWithViews(options =>
-{
-    options.Filters.Add<ValidationExceptionFilter>();
-    options.Filters.Add<GlobalExceptionFilter>();
-});
-
-builder.Services.AddDbContext<AppDbContext>(options =>
-{
-    options.UseNpgsql(
-        builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-
-builder.Services.AddInfrastructure();
-builder.Services.AddApplication();
-MapsterConfiguration.RegisterMappings();
-
 Log.Logger = new LoggerConfiguration()
     .MinimumLevel.Information()
+    .Enrich.FromLogContext()
     .WriteTo.Console()
     .WriteTo.File(
         "logs/taskflow-.txt",
         rollingInterval: RollingInterval.Day)
     .CreateLogger();
 
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+try
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Host.UseSerilog();
+
+    builder.Services.AddControllersWithViews(options =>
+    {
+        options.Filters.Add<ValidationExceptionFilter>();
+        options.Filters.Add<GlobalExceptionFilter>();
+    });
+
+    builder.Services.AddApplication();
+    builder.Services.AddInfrastructure();
+
+    MapsterConfiguration.RegisterMappings();
+
+    var app = builder.Build();
+
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Home/Error");
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseRouting();
+
+    app.UseAuthorization();
+
+    app.MapStaticAssets();
+
+    app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}")
+        .WithStaticAssets();
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapStaticAssets();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
-
-
-app.Run();
+catch (Exception exception)
+{
+    Log.Fatal(
+        exception,
+        "A aplicação foi finalizada inesperadamente.");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
